62de316d39e3d97f4498b7f5ab0b7b2a58a2e7a6
KIRIL <kirilpoposki@hotmail.com>
2022-11-23 13:48:38 +0100

    changing vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    public: '0.0.0.0:8080',
+  },
   transpileDependencies: ["vuetify"],
   configureWebpack: {
     module: {
